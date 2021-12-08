d5bc385f17703ae43b9e4ae60135cc8db19079da
Konrad Siek <konrad.siek@gmail.com>
2021-12-08 23:52:00 +0100

    Extra header for writeback.

---
+++ src/ufos_writeback.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum UfoWriteListenerEvent_Tag {
+  Writeback,
+  Reset,
+} UfoWriteListenerEvent_Tag;
+
+typedef struct Writeback_Body {
+  uintptr_t start_idx;
+  uintptr_t end_idx;
+  const uint8_t *data;
+} Writeback_Body;
+
+typedef struct UfoWriteListenerEvent {
+  UfoWriteListenerEvent_Tag tag;
+  union {
+    Writeback_Body writeback;
+  };
+} UfoWriteListenerEvent;
+
+typedef void (*UfoWritebackListener)(void *, struct UfoWriteListenerEvent);
